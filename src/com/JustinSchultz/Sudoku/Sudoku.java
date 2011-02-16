68e1f6d0f1c08310f3eed3bfc07b37869be3d71a
Justin Schultz <jmsconsulting@gmail.com>
2011-02-16 10:45:09 -0800

    First commit.

---
+++ src/com/JustinSchultz/Sudoku/Sudoku.java
@@ -0,0 +1,40 @@
+package com.JustinSchultz.Sudoku;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Sudoku extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        View continueButton = findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        
+        View newButton = findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        
+        View aboutButton = findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        
+        View exitButton = findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+    }
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+		case R.id.about_button:
+			Intent i = new Intent(this, About.class);
+			startActivity(i);
+			break;
+		}
+		
+	}
+}
